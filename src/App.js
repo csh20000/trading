3e4d9c147848872146e24140222fce4d4713077b
csh20000 <94726843+csh20000@users.noreply.github.com>
2022-05-01 19:15:49 -0400

    Menu icon needed

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ import { Box } from '@material-ui/core';
 import { Toolbar } from '@material-ui/core';
 import { IconButton } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
-import { MenuItem } from '@material-ui/core';
+
+//FIX
+import { MenuIcon } from '@material-ui/core';
+//import MenuIcon from '@material-ui/core';
 
 function App() {
   return (
     <div>
-      <p>hello world</p>
-      <button onClick={() => alert('clicked')}>hello button</button>
-      <Button onClick={() => alert('clicked')}>eee</Button>
-
       <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
@@ -26,14 +25,21 @@ function App() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
+            <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            i like one piece
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">House</Button>
+          <Button color="inherit">Slave</Button>
         </Toolbar>
       </AppBar>
     </Box>
+
+      <p>hello world</p>
+      <button onClick={() => alert('clicked')}>hello button</button>
+      <Button onClick={() => alert('clicked')}>eee</Button>
 
     </div>
   );
